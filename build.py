d7b2bb9679a1a27e9e36e72baad9e639122f1c4b
samet gozet <81412659+samet-g@users.noreply.github.com>
2022-04-11 21:44:06 +0300

    update broken tor link

--- build.py
+++ build.py
@@ -17,11 +17,11 @@ def download_tor():
     tor_url = 'https://www.torproject.org/dist/torbrowser/11.0.10/tor-win32-0.4.6.10.zip'
     wget.download(tor_url, out='C:')
     print(f"\n{bblue}[+]{bos} Tor is downloaded in C:\\ directory.")
-    with zipfile.ZipFile('C:\\tor-win32-0.4.6.9.zip', 'r') as tor:
+    with zipfile.ZipFile('C:\\tor-win32-0.4.6.10.zip', 'r') as tor:
         os.path.join("C:")
         tor.extractall('C:\\')
         print(f'{bblue}[+]{bos} File is unzipped in C: disk.')
-    pathlib.Path('C:\\tor-win32-0.4.6.9.zip').unlink()
+    pathlib.Path('C:\\tor-win32-0.4.6.10.zip').unlink()
     shutil.rmtree('C:\\Data')
     print(f"{bblue}[+]{bos} Zip file is gone.")
 
